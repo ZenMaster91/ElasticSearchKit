5bb5f01463164cd348390a8c7fd0d786b2b2eadf
Guillermo Facundo Colunga <colunga91@gmail.com>
2017-12-08 01:07:03 +0100

    Tested the vectorization of methods at ESQuery

--- src/test/java/ESKit/EKQueryTest.java
+++ src/test/java/ESKit/EKQueryTest.java
@@ -10,14 +10,14 @@ public class EKQueryTest {
 	public void test() {
 		ESKQuery statement = new ESKQuery( "select ? from ? where ? = cod.coche and ? = ?;" );
 		System.out.println( statement.asString() );
-		statement.setParam( "*" );
-		statement.setParam( "T.Empleados" );
-		statement.setParam( "cod.coche" );
-		statement.setParam( "nom.empleado" );
-		statement.setParam( "carlos" );
+		statement.setParam( "*" )
+			.setParam( "T.Empleados" )
+			.setParam( "cod.coche" )
+			.setParam( "nom.empleado" )
+			.setParam( "'carlos'" );
 		System.out.println( statement.asString() );
 		
-		assertEquals( "select * from T.Empleados where cod.coche = cod.coche and nom.empleado = carlos;", statement.asString() );
+		assertEquals( "select * from T.Empleados where cod.coche = cod.coche and nom.empleado = 'carlos';", statement.asString() );
 	}
 
 }
